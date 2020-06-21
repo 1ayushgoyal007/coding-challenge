b75874353a04aa8af76a8822bca7f95b87c2013a
Ayush Goyal <1ayushgoyal007@gmail.com>
2020-06-21 22:07:32 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.set('view engine','ejs');
+app.use(express.static(__dirname+"/public"));
+
+
+app.get("/",function(req,res){
+    res.render('index');
+})
+
+
+app.set("port",process.env.PORT || 5000 );
+
+app.listen(app.get("port"),function(req,res){
+    console.log("Server listening at 5000");
+})
